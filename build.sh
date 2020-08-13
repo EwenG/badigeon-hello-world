5a0548dd12dd15470e398de06e7555a5d86c4dd8
ewen <ewen.grosjean@free.fr>
2020-08-13 16:47:27 +0200

    init push

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+clojure -A:build -m hello.world.build
---
+++ src-build/hello/world/build.clj
@@ -0,0 +1,8 @@
+(ns hello.world.build
+  (:require [badigeon.clean :as clean]
+            [badigeon.javac :as javac]))
+
+(defn -main []
+  (clean/clean "target")
+  (javac/javac "src-java"))
+
